c57a25c9804eb1e8fd2991cc089b3f2366011a4a
MagL33To <stefan.benjamin@gmail.com>
2015-11-02 20:23:05 +0000

    controllers folder was untracked for some reason...

---
+++ PrimeTables/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PrimeTables.Code.Interfaces;
+
+namespace PrimeTables.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPrimeGenerator _primeGenerator;
+
+        public HomeController(IPrimeGenerator primeGenerator)
+        {
+            _primeGenerator = primeGenerator;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
