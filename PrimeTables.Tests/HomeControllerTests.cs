78ea48848b7259d548790a3432d24dd3e7ebd78e
MagL33To <stefan.benjamin@gmail.com>
2015-11-02 20:48:01 +0000

    Added get primes method on controller

---
+++ PrimeTables.Tests/HomeControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Moq;
+using NUnit.Framework;
+using PrimeTables.Code.Interfaces;
+using PrimeTables.Controllers;
+
+namespace PrimeTables.Tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private HomeController _fakeHomeController;
+        private Mock<IPrimeGenerator> _fakePrimeGenerator;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetup()
+        {
+            _fakePrimeGenerator = new Mock<IPrimeGenerator>();
+            _fakeHomeController = new HomeController(_fakePrimeGenerator.Object);
+        }
+
+        [Test]
+        public void GetPrimes_ValidIntegerSupplied_ReturnJsonResult()
+        {
+            const int count = 5;
+            var list = Enumerable.Range(0, count).ToList();
+
+            _fakePrimeGenerator.Setup(x => x.GenerateNumberOfPrimes(It.IsAny<int>()))
+                .Returns(list);
+
+            var result = _fakeHomeController.GetPrimes(count);
+
+            Assert.IsInstanceOf<JsonResult>(result);
+        }
+
+        [Test]
+        public void GetPrimes_ValidIntegerSupplied_NonNullDataReturned()
+        {
+            const int count = 5;
+            var list = Enumerable.Range(0, count).ToList();
+
+            _fakePrimeGenerator.Setup(x => x.GenerateNumberOfPrimes(It.IsAny<int>()))
+                .Returns(list);
+
+            var result = _fakeHomeController.GetPrimes(count);
+
+            Assert.That(result.Data, Is.Not.Null);
+        }
+    }
+}
--- PrimeTables/Controllers/HomeController.cs
+++ PrimeTables/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using PrimeTables.Code.Interfaces;
 
 namespace PrimeTables.Controllers
@@ -15,9 +11,14 @@ namespace PrimeTables.Controllers
         {
             _primeGenerator = primeGenerator;
         }
-        public ActionResult Index()
+        public ViewResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetPrimes(int count)
+        {
+            return Json(_primeGenerator.GenerateNumberOfPrimes(count));
         }
     }
 }
